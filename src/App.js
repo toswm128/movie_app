7960e64d3c068873ff76c70fd4cfe7d76c8337c5
toswm128 <chominsu10@gmail.com>
2021-02-10 23:07:15 +0900

    Router but now without navigator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import About from "./routes/About"
 import Home from "./routes/Home"
+import { HashRouter, Route } from "react-router-dom";
 
 function App() {
-  return <Home />
+  return( 
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+    )
 }
 
 export default App;
